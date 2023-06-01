abeb02ae7093f7967a5c2476cd183b78b87ad07a
chenweiyan <chanweiyan007@gmail.com>
2023-06-01 18:40:27 +0800

    feat: nest g resource person

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AaaModule } from './aaa/aaa.module';
 import { XxxModule } from './xxx/xxx.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [AaaModule, XxxModule],
+  imports: [AaaModule, XxxModule, PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
