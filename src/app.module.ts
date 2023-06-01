618f33c3f44ad663f6c0c63e1596b3b21b8bd4eb
chenweiyan <chanweiyan007@gmail.com>
2023-06-01 17:59:44 +0800

    feat: nest g resource xxx

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AaaModule } from './aaa/aaa.module';
+import { XxxModule } from './xxx/xxx.module';
 
 @Module({
-  imports: [AaaModule],
+  imports: [AaaModule, XxxModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3009;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // await app.listen(3000);
-  await app.listen(3009);
+  await app.listen(PORT);
+  console.log(`visit http://localhost:${PORT}`);
 }
 bootstrap();
