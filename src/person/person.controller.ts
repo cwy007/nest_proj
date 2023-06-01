357100c9680f5f9f3e19520c227a1016dfd7cab2
chenweiyan <chanweiyan007@gmail.com>
2023-06-01 20:50:15 +0800

    feat: urlParam

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -11,9 +11,14 @@ import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 
-@Controller('person')
+@Controller('api/person')
 export class PersonController {
   constructor(private readonly personService: PersonService) {}
+
+  // @Get(':id')
+  // urlParam(@Param('id') id: string) {
+  //   return `received: id=${id}`;
+  // }
 
   @Post()
   create(@Body() createPersonDto: CreatePersonDto) {
