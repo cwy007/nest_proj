a9228b21e9ed569cbab4528bba7bc924f0a45a07
chenweiyan <chanweiyan007@gmail.com>
2023-06-01 19:23:08 +0800

    feat: useStaticAssets

---
+++ docs/api.md
@@ -0,0 +1,14 @@
+# api
+
+## 静态资源
+
+```tsx
+
+// 文件和接口路径的对应关系
+// public/index.html
+/static
+
+// publib/foo.html
+/static/foo
+
+```
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 const PORT = 3009;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static' });
   await app.listen(PORT);
   console.log(`visit http://localhost:${PORT}`);
 }
