6677aeab0c1f7c2248809b1760d598e1863f2baa
chanweiyan <chanweiyan007@gmail.com>
2024-09-29 19:28:50 +0800

    ✨ feat: 使用多种 Provider，灵活注入对象
    灵活运用这些 provider 类型，就可以利用 Nest 的 IOC 容器注入任何对象
    
    https://juejin.cn/book/7226988578700525605/section/7227379507152781349

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('app_service') private readonly appService2: AppService,
+    @Inject('person') private readonly person: { name: string; age: number },
+    @Inject('person2') private readonly person2: { name: string; age: number },
+    @Inject('person3') private readonly person3: { name: string; desc: number },
+    @Inject('person4') private readonly person4: { name: string; desc: number },
+    @Inject('fooo') private readonly fooo: { name: string; desc: number },
+  ) {}
 
   @Get()
   getHello(): string {
     console.log(this);
+    // debugger
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,64 @@ import { PersonModule } from './person/person.module';
 import { BookModule } from './book/book.module';
 import { Person2Module } from './person2/person2.module';
 
+// 这些自定义 provider 的方式里，最常用的是 useClass，不过我们一般会用简写，也就是直接指定 class。
+// useClass 的方式由 IoC 容器负责实例化，我们也可以用 useValue、useFactory 直接指定对象。
+// useExisting 只是用来起别名的，有的场景下会用到。
+
 @Module({
   imports: [AaaModule, XxxModule, PersonModule, BookModule, Person2Module],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'cwy007',
+        age: 32,
+      }
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'cgs007',
+          age: 58,
+        }
+      }
+    },
+    {
+      provide: 'person3',
+      // 这个 useFactory 支持通过参数注入别的 provider：
+      useFactory(person: { name: string }, appService: AppService) {
+        return {
+          name: person.name,
+          desc: appService.getHello()
+        }
+      },
+      inject: ['person', AppService], // 指定 useFactory 的参数
+    },
+    // useFactory 支持异步
+    {
+      provide: 'person4',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+        return {
+          name: 'bbb',
+          desc: 'ccc',
+        }
+      },
+    },
+    // provider 还可以通过 useExisting 来指定别名
+    {
+      provide: 'fooo', // 这个是别名
+      useExisting: 'person4',
+    }
+  ],
 })
 export class AppModule {}
