688690f6e828d7664e5db7ea1840da979abf3b84
chanweiyan <chanweiyan007@gmail.com>
2026-05-04 21:48:47 +0800

    fix: test user CRUD api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  @InjectEntityManager()
+  private readonly manager: EntityManager;
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    this.manager.save(User, createUserDto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.manager.find(User);
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.manager.findOne(User, { where: { id } });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.manager.save(User, {
+      id,
+      ...updateUserDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.manager.delete(User, id);
   }
 }
