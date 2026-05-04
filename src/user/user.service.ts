84a3fd234ffd76631b4039b5c501b9079cf9f63c
chanweiyan <chanweiyan007@gmail.com>
2026-05-04 21:56:18 +0800

    fix: InjectRepository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectEntityManager } from '@nestjs/typeorm';
-import { EntityManager } from 'typeorm';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+
   @InjectEntityManager()
   private readonly manager: EntityManager;
+
+  @InjectRepository(User)
+  private readonly userRepository: Repository<User>;
 
   create(createUserDto: CreateUserDto) {
     this.manager.save(User, createUserDto);
   }
 
   findAll() {
-    return this.manager.find(User);
+    // return this.manager.find(User);
+    return this.userRepository.find();
   }
 
   findOne(id: number) {
