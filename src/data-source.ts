917d524572bacb05c921d8523bed9184ce243aeb
chanweiyan <chanweiyan007@gmail.com>
2026-05-05 18:28:53 +0800

    fix: user dotenv to read config value from src/.env from src/data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,18 @@
 import { DataSource } from "typeorm";
 import { Article } from "./article/entities/article.entity";
+import { config } from "dotenv";
+
+config({ path: 'src/.env' });
+
+console.log(process.env);
 
 export default new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Cwy17824',
-  database: 'typeorm_migration_test',
+  host: process.env.mysql_server_host,
+  port: Number(process.env.mysql_server_port),
+  username: process.env.mysql_server_username,
+  password: process.env.mysql_server_password,
+  database: process.env.mysql_server_database,
   synchronize: false,
   logging: true,
   entities: [Article],
